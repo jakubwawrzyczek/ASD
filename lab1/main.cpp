0cfb41463f15d64296144d4e9c4e776d086003aa
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-10-03 09:27:42 +0200

    .

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,21 +2,58 @@
 using namespace std;
 
 struct Osoba {
-public:
+    // dane
+    string imie;
+    string nazwisko;
+    basic_string<char> wiek{};
+
+    // wskaznik na nastepna osobe
+    Osoba* nastepnaOsoba;
+
     Osoba(string i, string n, int w) {
         this->imie = i;
         this->nazwisko = n;
         this->wiek = w;
+    }
+
+    Osoba () {
+        nastepnaOsoba = NULL;
+    }
+};
+
+struct Lista {
+    // wskaznik na pierwsza osobe w liscie
+    Osoba *pierwszaOsoba = NULL;
+
+    // konstruktor listy
+    Lista() {
+        pierwszaOsoba = NULL;
+    }
+    Lista(Osoba* x) {
+        pierwszaOsoba = x;
+    }
+
+    // dodawanie osoby
+    void dodaj(string i, string n, int w) {
+        Osoba* nowaOsoba = new Osoba(i, n, w);
+
+        if(pierwszaOsoba == NULL) {
+            pierwszaOsoba = nowaOsoba;
+            return;
+        }
+
+        Osoba* temp = pierwszaOsoba;
+        while (temp->nastepnaOsoba == NULL) {
+            temp = temp->nastepnaOsoba;
+        }
+        temp->nastepnaOsoba = nowaOsoba;
+    }
+
+    // wyswietlanie listy
+    void wyswietlListe() {
 
     }
-private:
-    // dane
-    string imie;
-    string nazwisko;
-    int wiek{};
 
-    // wskaznik na nastepna osobe
-    struct Osoba* nastepna_osoba{};
 };
 
 
