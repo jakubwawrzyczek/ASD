11493a45532ea3620fb417c86d2ae6a1a829a57e
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-11-18 21:02:58 +0100

    .

--- drzewa/main.cpp
+++ drzewa/main.cpp
@@ -288,7 +288,33 @@ void rotate(Node* &root, string side) {
         }
 
     } else if (side == "L") {
-        
+        // jezeli nie ma prawego syna to nie obrocimy w prawo
+        if (!root->R) return;
+
+        if (root->R->L) {
+            // zapisuje aktualnego lewego syna przyszlego roota
+            Node* temp = root->R->L;
+
+            // obecny root staje sie lewym synem przyszlego roota
+            root->R->L = root;
+
+            // prawy syn obecnego roota zostaje nowym rootem
+            root = root->R;
+
+            // ustawiam parentow zeby wszystko sie zgadzalo
+            root->P = nullptr;
+            root->L->P = root;
+            temp->P = root->L;
+
+            // lewy syn nowego roota idzie jako prawy syn starego roota
+            root->L->R = temp;
+        } else {
+            root->R->L = root;
+            root = root->R;
+            root->L->P = root;
+            root->P= nullptr;
+            root->L->R = nullptr;
+        }
 
     } else {
         cout << "Niepoprawna wartosc zmiennej side, wybierz \"L\" lub \"R\"" << endl;
