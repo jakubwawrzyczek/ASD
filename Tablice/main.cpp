1124a699cf8c37c40f7c8cfe60889ebe118c5744
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-12-04 18:59:51 +0100

    .

--- Tablice/main.cpp
+++ Tablice/main.cpp
@@ -25,7 +25,17 @@ void bubbleSort(int* arr, int len) {
 
 }
 
+void swap(int* arr, int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
 
+void heapify(int* arr, int n, int heapsize) {
+    int L = 2*n;
+    int R = 2*n+1;
+    int largest = 0;
+}
 
 int main() {
     int tab[24] = {1, 2, 4, 6, 3, 2, 5, -3, 4 -12, -4, 55, 123, -23, -44, 0, -54, -56, -2003, -200, 20, 69, 60, 420};
