d92d27ccdbc0aeefbd4feef0603fb7ec70d19593
Jakub Wawrzyczek <jakubwawrzyczek.contact@gmail.com>
2023-12-04 19:12:06 +0100

    .

--- Tablice/main.cpp
+++ Tablice/main.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void printArray(int* arr, int size) {
     for (int i = 0; i < size; i++) {
-        std::cout << arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    std::cout << std::endl;
+    cout << endl;
 }
 
 void bubbleSort(int* arr, int len) {
@@ -35,10 +35,45 @@ void heapify(int* arr, int n, int heapsize) {
     int L = 2*n;
     int R = 2*n+1;
     int largest = 0;
+
+    if (L <= heapsize && arr[L] > arr[n]) {
+        largest = L;
+    }
+    else {
+        largest = n;
+    }
+
+    if (R <= heapsize && arr[R] > arr[largest]) {
+        largest = R;
+    }
+
+    if (largest != n) {
+        swap(arr, largest, n);
+        heapify(arr, largest, heapsize);
+    }
+}
+
+void buildHeap(int* arr, int heapsize) {
+    for (int i = heapsize / 2 ; i < 0 ; i--) {
+        heapify(arr, i, heapsize);
+    }
+}
+
+void heapSort(int* arr, int heapsize) {
+    buildHeap(arr, heapsize);
+
+    while (heapsize > 0) {
+        swap(arr, 1, heapsize);
+        heapsize--;
+        heapify(arr, 1, heapsize);
+    }
 }
 
 int main() {
-    int tab[24] = {1, 2, 4, 6, 3, 2, 5, -3, 4 -12, -4, 55, 123, -23, -44, 0, -54, -56, -2003, -200, 20, 69, 60, 420};
+    int arr[24] = {NULL, 1, 2, 4, 6, 3, 2, 5, -3, 4 -12, -4, 55, 123, -23, -44, 0, -54, -56, -2003, -200, 20, 69, 60, 420};
 
+    heapSort(arr, 24);
+
+    printArray(arr, 24);
 
 }
